51ce68abad8b7d8598086278e35c29b07052a163
qazwini <mmqazwini@yahoo.com>
2021-03-12 20:56:43 -0500

    Add title and input field

--- main.swift
+++ main.swift
@@ -7,7 +7,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var window: NSWindow?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        window = NSWindow(contentRect: NSMakeRect(10, 10, 400, 300), styleMask: [.titled, .resizable, .closable], backing: .buffered, defer: false)
+        window = NSWindow(contentRect: NSMakeRect(10, 10, 400, 300), styleMask: [.titled, .closable], backing: .buffered, defer: false)
         let content = window!.contentView! as NSView
         let vc = ViewController()
         content.addSubview(vc.view)
@@ -21,9 +21,52 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 }
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTextFieldDelegate {
+    
+    let titleLabel = NSTextField()
+    let inputField = NSTextField()
+    let displayLabel = NSTextField()
+    
     override func loadView() {
+        setupUI()
+        inputField.delegate = self
     }
+    
+    func setupUI() {
+        self.view = NSView(frame: NSMakeRect(0, 0, 400, 300))
+        
+        labelify(titleLabel)
+        titleLabel.stringValue = "Welcome to the Mustang Bronco Program!"
+        
+        labelify(displayLabel)
+
+        [titleLabel, inputField, displayLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            
+            inputField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            inputField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            inputField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
+        ])
+    }
+    
+    func labelify(_ label: NSTextField) {
+        label.isBezeled = false
+        label.drawsBackground = false
+        label.isEditable = false
+        label.isSelectable = false
+    }
+
+    func controlTextDidChange(_ obj: Notification) {
+        displayLabel.stringValue = inputField.stringValue
+    }
+    
 }
 
 let app = NSApplication.shared
